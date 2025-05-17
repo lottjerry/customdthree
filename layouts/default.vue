93153c472b1c438fd40f816b4f04fa71a59bbea7
Jerry Lott <lottjerry@gmail.com>
2025-05-17 13:51:05 -0500

    Added gsap, prettier, and tailwind

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="relative flex h-dvh flex-col items-center justify-center bg-black p-5 border"
+  >
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup></script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div v-gsap.entrance.slide-left class="border">
+    <h1 class="p-5 text-white">Index Page</h1>
+  </div>
+</template>
+
+<script setup></script>
