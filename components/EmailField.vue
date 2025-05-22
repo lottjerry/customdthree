66243b19a1dad9193a3d311c068598cbea4a3e38
Jerry Lott <lottjerry@gmail.com>
2025-05-22 03:08:33 -0500

    Added validation to email submission

--- components/EmailField.vue
+++ components/EmailField.vue
@@ -1,31 +1,51 @@
 <template>
   <div class="flex w-full flex-col items-center gap-2">
     <h3 class="w-3/4 text-lg">Email *</h3>
-    <div
-      class="border-colors flex h-14 w-3/4 items-center justify-between rounded-3xl border bg-transparent px-1"
+    <form
+      @submit="onSubmit"
+      class="border-colors flex h-14 w-3/4 items-center justify-between rounded-3xl border bg-transparent px-1 "
+      :class="errors.email ? 'border-red-600' : 'border-white'"
     >
       <input
-        id="email"
         v-model="email"
-        type="text"
+        v-bind="emailAttrs"
         class="ml-5 h-8 w-72 bg-transparent text-lg outline-none"
         placeholder="example@domain.com"
       />
       <button
-        class="mr-3 h-12 w-32 rounded-3xl border text-lg transition-transform duration-700 ease-in-out hover:translate-x-3"
+        :disabled="errors.email"
+        class="mr-3 h-12 w-32 rounded-3xl border text-lg transition-transform duration-700 ease-in-out hover:translate-x-3 disabled:opacity-50 disabled:translate-x-0 disabled:cursor-not-allowed disabled:border-red-600"
+        :class="errors.email ? '' : 'button-animation'"
       >
         <span>Notify Me!</span>
       </button>
-    </div>
+    </form>
+    <p class="w-3/4 text-red-600 absolute bottom-4">{{ errors.email }}</p>
   </div>
 </template>
 
 <script setup>
-  const email = ref('');
+  import * as yup from 'yup';
+  import { useForm } from 'vee-validate';
+
+  const { errors, handleSubmit, defineField } = useForm({
+    validationSchema: yup.object({
+      email: yup
+        .string()
+        .email('Please enter a valid email.')
+        .required('Please enter an email.'),
+    }),
+  });
+
+  const [email, emailAttrs] = defineField('email');
+
+  const onSubmit = handleSubmit((values) => {
+    alert(JSON.stringify(values, null, 2));
+  });
 </script>
 
 <style scoped>
-  button:hover {
+  .button-animation:hover {
     animation: gradientAnimation 15s linear infinite;
     background: linear-gradient(
       90deg,
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'v-gsap-nuxt', '@nuxt/icon'],
+  modules: ['@nuxtjs/tailwindcss', 'v-gsap-nuxt', '@nuxt/icon', '@vee-validate/nuxt'],
   icon: {
     customCollections: [
       {
