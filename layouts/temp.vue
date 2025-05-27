bd2ad86b594482ce310ed3d52e914dc7e6e23079
Jerry Lott <lottjerry@gmail.com>
2025-05-26 22:57:15 -0500

    Added more spacing to nav items

--- layouts/temp.vue
+++ layouts/temp.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="h-dvh bg-black">
     <header class="fixed left-0 top-0 h-[6rem] w-dvw bg-black">
-      <nav class="flex h-full items-center justify-around text-white">
-        <IconLogo />
+      <nav class="flex h-full items-center justify-between text-white md:justify-around md:gap-36">
+        <IconLogo class="ml-5 md:ml-0" />
         <GreetingButton />
       </nav>
     </header>
