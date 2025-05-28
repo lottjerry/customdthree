e53eb418a601abe90da90e3b8e2d5b025969b985
Jerry Lott <lottjerry@gmail.com>
2025-05-28 03:38:51 -0500

    Finished responsive design for CountdownTimer component.

--- components/CountdownTimer.vue
+++ components/CountdownTimer.vue
@@ -2,52 +2,54 @@
   <div class="text-white">
     <client-only>
       <VueCountdown
-        class="flex items-start justify-center gap-8"
+        class="flex items-start justify-center md:gap-8 gap-2"
         :time="190 * 24 * 60 * 60 * 1000"
         :transform="transformSlotProps"
         v-slot="{ days, hours, minutes, seconds }"
       >
         <!-- Days -->
         <div class="flex flex-col items-center">
-          <h1 class="text-7xl font-thin">{{ days }}</h1>
-          <h3 class="mt-2 text-2xl">Days</h3>
+          <h1 class="md:text-7xl text-5xl font-thin">{{ days }}</h1>
+          <h3 class="mt-2 md:text-2xl textxl">Days</h3>
         </div>
 
         <!-- Colon -->
-        <div class="mt-6 flex flex-col justify-center gap-3">
-          <div class="first-colon h-2 w-2 rounded-full"></div>
-          <div class="first-colon h-2 w-2 rounded-full"></div>
+        <div class="md:mt-6 mt-3 flex flex-col justify-center md:gap-3 gap-2">
+          <div class="first-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
+           <div class="first-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
         </div>
 
         <!-- Hours -->
         <div class="flex flex-col items-center">
-          <h1 class="text-7xl font-thin">{{ hours }}</h1>
-          <h3 class="mt-2 text-2xl">Hours</h3>
+          <h1 class="md:text-7xl text-5xl font-thin">{{ hours }}</h1>
+          <h3 class="mt-2 md:text-2xl textxl">Hours</h3>
         </div>
 
         <!-- Colon -->
-        <div class="mt-6 flex flex-col justify-center gap-3">
-          <div class="second-colon h-2 w-2 rounded-full"></div>
-          <div class="second-colon h-2 w-2 rounded-full"></div>
+        <div class="md:mt-6 mt-3 flex flex-col justify-center md:gap-3 gap-2">
+          <div class="second-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
+           <div class="second-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
         </div>
 
         <!-- Minutes -->
         <div class="flex flex-col items-center">
-          <h1 class="text-7xl font-thin">{{ minutes }}</h1>
-          <h3 class="mt-2 text-2xl">Mins.</h3>
+          <h1 class="md:text-7xl text-5xl font-thin">{{ minutes }}</h1>
+          <h3 class="mt-2 md:text-2xl textxl">Minutes</h3>
         </div>
 
+
         <!-- Colon -->
-        <div class="mt-6 flex flex-col justify-center gap-3">
-          <div class="third-colon h-2 w-2 rounded-full"></div>
-          <div class="third-colon h-2 w-2 rounded-full"></div>
+        <div class="md:mt-6 mt-3 flex flex-col justify-center md:gap-3 gap-2">
+          <div class="third-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
+           <div class="third-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
         </div>
 
         <!-- Seconds -->
         <div class="flex flex-col items-center">
-          <h1 class="text-7xl font-thin">{{ seconds }}</h1>
-          <h3 class="mt-2 text-2xl">Secs.</h3>
+          <h1 class="md:text-7xl text-5xl font-thin">{{ seconds }}</h1>
+          <h3 class="mt-2 md:text-2xl textxl">Seconds</h3>
         </div>
+
       </VueCountdown>
     </client-only>
   </div>
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <section
-      class="flex h-dvh flex-col items-start justify-center gap-10 border-[2px] px-5 md:items-center md:px-0"
+      class="flex h-dvh flex-col items-start justify-center gap-10 border-[2px] px-2 md:items-center md:px-0 mt-14 md:mt-0"
     >
       <Subtitle />
       <HeroTitle />
+      <CountdownTimer />
     </section>
     <section class="h-dvh border-[2px]"></section>
   </div>
