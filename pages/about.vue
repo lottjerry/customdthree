8ab1d4618fb3fa982ae532154da2124db8ddff83
Jerry Lott <lottjerry@gmail.com>
2025-05-28 03:49:41 -0500

    Finished responsive design for scroll down animation.

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <section
-      class="flex h-dvh flex-col items-start justify-center gap-10 border-[2px] px-2 md:items-center md:px-0 mt-14 md:mt-0"
+      class="flex h-dvh flex-col items-start justify-center gap-10 border-[2px] px-2 md:items-center md:px-0"
     >
       <Subtitle />
       <HeroTitle />
       <CountdownTimer />
+      <div class="absolute md:bottom-[5em] md:left-[5em] bottom-[1em] left-[2em] w-[1.5rem] md:w-auto">
+        <client-only>
+          <Vue3Lottie :animationLink="animationLink" :height="80" />
+        </client-only>
+      </div>
     </section>
     <section class="h-dvh border-[2px]"></section>
   </div>
@@ -15,4 +20,7 @@
   definePageMeta({
     layout: 'temp',
   });
+  const animationLink = ref(
+    'https://lottie.host/e0804992-47f3-420d-b30c-412a734c0cc0/cFcHTrYcci.json',
+  );
 </script>
