346edd31b9ea11f4614d0e0871a14d410ca42483
Jerry Lott <lottjerry@gmail.com>
2025-06-01 11:29:37 -0500

    Changed code to index and default. Deleted unnecessary files.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="bg-black"
-  >
-    <main>
+  <div class="h-dvh bg-black">
+    <header class="fixed left-0 top-0 h-[6rem] w-dvw bg-black z-30">
+      <nav class="flex h-full items-center justify-between text-white md:justify-around md:gap-36">
+        <IconLogo class="ml-5 md:ml-0" />
+        <GreetingButton />
+      </nav>
+    </header>
+    <main class="bg-black">
       <slot />
     </main>
   </div>
 </template>
-
-<script setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,26 @@
 <template>
   <div>
-    <header class="fixed start-0 top-0 z-20">
-      <Navbar />
-    </header>
     <section
-      class="align-center relative mb-[100px] flex h-dvh flex-col items-center pt-[200px]"
+      class="flex h-dvh flex-col items-start justify-center gap-10  px-2 md:items-center md:px-0"
     >
-      <div class="flex flex-col items-center justify-center gap-[50px]">
-        <Subtitle class="relative z-20" />
-        <div>
-          <HeroTitle />
-        </div>
-
-        <CountdownTimer class="relative z-20 mt-[4em]" />
-      </div>
-      <div class="absolute bottom-[5em] left-[5em]">
+      <Subtitle />
+      <HeroTitle />
+      <CountdownTimer />
+      <div
+        class="absolute bottom-[1em] left-[2em] w-[1.5rem] md:bottom-[5em] md:left-[5em] md:w-auto"
+      >
         <client-only>
           <Vue3Lottie :animationLink="animationLink" :height="80" />
         </client-only>
       </div>
     </section>
     <section
-      class="no-repeat relative z-10 flex h-dvh flex-col items-center justify-center gap-32 bg-[url(/assets/images/background.jpeg)] bg-contain bg-center bg-no-repeat text-white bg-fixed"
+      class="relative h-dvh  bg-[url(/assets/images/Background_Mobile.jpeg)] md:bg-[url(/assets/images/Background_Tablet.jpeg)] lg:bg-[url(/assets/images/Background_Full.jpeg)] bg-cover bg-fixed bg-center bg-no-repeat flex flex-col items-center justify-center px-1 md:px-0"
     >
-      <GlassCard />
+    <GlassCard />
       <footer class="absolute bottom-0">
-        <Footer />
-      </footer>
+      <Footer />
+    </footer>
     </section>
   </div>
 </template>
@@ -36,5 +30,3 @@
     'https://lottie.host/e0804992-47f3-420d-b30c-412a734c0cc0/cFcHTrYcci.json',
   );
 </script>
-
-<style scoped></style>
