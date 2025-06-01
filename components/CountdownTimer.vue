1740898491c5d90d96b97699fa141e7770648757
Jerry Lott <lottjerry@gmail.com>
2025-06-01 09:58:05 -0500

    Minor adjustments to the Coutndown Timer.

--- components/CountdownTimer.vue
+++ components/CountdownTimer.vue
@@ -2,54 +2,64 @@
   <div class="text-white">
     <client-only>
       <VueCountdown
-        class="flex items-start justify-center md:gap-8 gap-2"
+        class="flex items-start justify-center md:gap-8 gap-3"
         :time="190 * 24 * 60 * 60 * 1000"
         :transform="transformSlotProps"
         v-slot="{ days, hours, minutes, seconds }"
       >
         <!-- Days -->
-        <div class="flex flex-col items-center">
-          <h1 class="md:text-7xl text-5xl font-thin">{{ days }}</h1>
-          <h3 class="mt-2 md:text-2xl textxl">Days</h3>
+        <div class="flex flex-col items-center w-15">
+          <h1 class="text-4xl font-thin md:text-7xl">{{ days }}</h1>
+          <h3 class="mt-2 md:text-2xl">Days</h3>
         </div>
 
         <!-- Colon -->
-        <div class="md:mt-6 mt-3 flex flex-col justify-center md:gap-3 gap-2">
-          <div class="first-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
-           <div class="first-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
+        <div class="mt-3 flex flex-col justify-center gap-2 md:mt-6 md:gap-3">
+          <div
+            class="first-colon h-[0.3rem] w-[0.3rem] rounded-full md:h-2 md:w-2"
+          ></div>
+          <div
+            class="first-colon h-[0.3rem] w-[0.3rem] rounded-full md:h-2 md:w-2"
+          ></div>
         </div>
 
         <!-- Hours -->
-        <div class="flex flex-col items-center">
-          <h1 class="md:text-7xl text-5xl font-thin">{{ hours }}</h1>
-          <h3 class="mt-2 md:text-2xl textxl">Hours</h3>
+        <div class="flex flex-col items-center w-15">
+          <h1 class="text-4xl font-thin md:text-7xl">{{ hours }}</h1>
+          <h3 class="textxl mt-2 md:text-2xl">Hours</h3>
         </div>
 
         <!-- Colon -->
-        <div class="md:mt-6 mt-3 flex flex-col justify-center md:gap-3 gap-2">
-          <div class="second-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
-           <div class="second-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
+        <div class="mt-3 flex flex-col justify-center gap-2 md:mt-6 md:gap-3">
+          <div
+            class="second-colon h-[0.3rem] w-[0.3rem] rounded-full md:h-2 md:w-2"
+          ></div>
+          <div
+            class="second-colon h-[0.3rem] w-[0.3rem] rounded-full md:h-2 md:w-2"
+          ></div>
         </div>
 
         <!-- Minutes -->
-        <div class="flex flex-col items-center">
-          <h1 class="md:text-7xl text-5xl font-thin">{{ minutes }}</h1>
-          <h3 class="mt-2 md:text-2xl textxl">Minutes</h3>
+        <div class="flex flex-col items-center  w-15">
+          <h1 class="text-4xl font-thin md:text-7xl">{{ minutes }}</h1>
+          <h3 class="textxl mt-2 md:text-2xl">Minutes</h3>
         </div>
 
-
         <!-- Colon -->
-        <div class="md:mt-6 mt-3 flex flex-col justify-center md:gap-3 gap-2">
-          <div class="third-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
-           <div class="third-colon md:h-2 md:w-2 h-[0.4rem] w-[0.4rem] rounded-full"></div>
+        <div class="mt-3 flex flex-col justify-center gap-2 md:mt-6 md:gap-3">
+          <div
+            class="third-colon h-[0.3rem] w-[0.3rem] rounded-full md:h-2 md:w-2"
+          ></div>
+          <div
+            class="third-colon h-[0.3rem] w-[0.3rem] rounded-full md:h-2 md:w-2"
+          ></div>
         </div>
 
         <!-- Seconds -->
-        <div class="flex flex-col items-center">
-          <h1 class="md:text-7xl text-5xl font-thin">{{ seconds }}</h1>
-          <h3 class="mt-2 md:text-2xl textxl">Seconds</h3>
+        <div class="flex flex-col items-center w-15">
+          <h1 class="text-4xl font-thin md:text-7xl">{{ seconds }}</h1>
+          <h3 class="textxl mt-2 md:text-2xl">Seconds</h3>
         </div>
-
       </VueCountdown>
     </client-only>
   </div>
