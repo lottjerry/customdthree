9c97a7a2fb60fbd1a8d9c92141dc7fbbcdac99de
Jerry Lott <lottjerry@gmail.com>
2025-06-02 13:12:54 -0500

    Minor change to height on mobile devices.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-locomotive>
     <section
-      class="flex h-dvh flex-col items-start justify-center gap-10 px-2 md:items-center md:px-0"
+      class="flex lg:h-dvh h-svh flex-col items-start justify-center gap-10 px-2 md:items-center md:px-0"
       data-scroll-section
     >
       <Subtitle />
@@ -18,7 +18,7 @@
     </section>
     <section
       data-scroll-section
-      class="relative flex h-dvh flex-col items-center justify-center bg-[url(/assets/images/Background_Mobile.jpeg)] bg-cover bg-fixed bg-center bg-no-repeat px-1 md:bg-[url(/assets/images/Background_Tablet.jpeg)] md:px-0 lg:bg-[url(/assets/images/Background_Full.jpeg)]"
+      class="relative flex lg:h-dvh h-svh flex-col items-center justify-center bg-[url(/assets/images/Background_Mobile.jpeg)] bg-cover bg-fixed bg-center bg-no-repeat px-1 md:bg-[url(/assets/images/Background_Tablet.jpeg)] md:px-0 lg:bg-[url(/assets/images/Background_Full.jpeg)]"
     >
       <GlassCard />
       <footer class="absolute bottom-0">
