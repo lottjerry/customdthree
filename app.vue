83d2c647b101cc785b30720418ef74fe612b5bd7
Jerry Lott <lottjerry@gmail.com>
2025-06-06 11:52:12 -0500

    Added meta data.

--- app.vue
+++ app.vue
@@ -9,6 +9,18 @@
 
 <script setup>
   const showLoader = ref(true);
+
+  useHead({
+    title: 'Custom D3',
+    meta: [{ name: "Custom D3", content: "Coming soon page for a web design and development company.", keywords: "Coming soon, web, development, deployment, design, custom", robots: "noindex, nofollow", Language: "english", author: "Jerry Lott" }],
+  })
+
+  useSeoMeta({
+  title: 'Custom D3',
+  ogTitle: 'Custom D3',
+  description:  "Coming soon page for a web design and development company.",
+  ogDescription:  "Coming soon page for a web design and development company.",
+})
 </script>
 
 <style>
