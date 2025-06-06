00b7ba57f7ba56cd80deb8018b636a2a402f504e
Jerry Lott <lottjerry@gmail.com>
2025-06-06 11:34:05 -0500

    Added legal documents to footer.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="flex md:h-[150px] items-center justify-center gap-5">
-    <p class="text-white">© 2025 Custom D3</p>
+  <div class="flex items-center justify-center gap-5 md:h-[150px]">
+    <div class="flex flex-col items-center text-white">
+      <p class="text-xs md:text-base">© 2025 Custom D3</p>
+      <div class="flex gap-2 text-xs md:text-base">
+        <NuxtLink
+          to="https://www.termsfeed.com/live/b812a0da-8ee1-4267-82de-1215a94d8fa7"
+          target="_blank"
+          >Privacy Policy</NuxtLink
+        >
+        <div>|</div>
+        <NuxtLink
+          to="https://www.termsfeed.com/live/b85ae36b-6369-4a77-8094-1b07b3941c48"
+          target="_blank"
+          >Terms & Conditions</NuxtLink
+        >
+      </div>
+    </div>
+
     <div>
       <client-only>
         <Vue3Lottie :animationLink="animationLink" :height="110" />
