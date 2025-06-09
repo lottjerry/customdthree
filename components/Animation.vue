73c62629fb56f8aac7418dd18815913eb2469af6
Jerry Lott <lottjerry@gmail.com>
2025-06-08 19:45:57 -0500

    Added animation component. Fixed loading bar animation on safari and ios devices.

---
+++ components/Animation.vue
@@ -0,0 +1,285 @@
+<template>
+  <div class="h-[72px] md:h-[112px]">
+    <div class="loader-wrapper">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .loader-wrapper {
+    display: flex;
+    align-items: center; /* align to center vertically */
+    gap: 5px;
+    width: max-content;
+    height: 50px;
+  }
+
+  .bar {
+    transform: scaleY(0.06);
+    transform-origin: center center;
+    background-color: #white;
+    width: 10px;
+    height: 50px;
+  }
+
+  .bar:nth-child(1) {
+    animation:
+      spike1 0.65s linear infinite alternate,
+      colorShift1 4s linear infinite;
+  }
+  .bar:nth-child(2) {
+    animation:
+      spike2 0.65s linear infinite alternate,
+      colorShift2 4s linear infinite;
+  }
+  .bar:nth-child(3) {
+    animation:
+      spike3 0.65s linear infinite alternate,
+      colorShift3 4s linear infinite;
+  }
+  .bar:nth-child(4) {
+    animation:
+      spike4 0.65s linear infinite alternate,
+      colorShift4 4s linear infinite;
+  }
+  .bar:nth-child(5) {
+    animation:
+      spike5 0.65s linear infinite alternate,
+      colorShift5 4s linear infinite;
+  }
+  .bar:nth-child(6) {
+    animation:
+      spike6 0.65s linear infinite alternate,
+      colorShift6 4s linear infinite;
+  }
+
+  @keyframes spike1 {
+    0% {
+      transform: scaleY(0.06);
+    }
+    16% {
+      transform: scaleY(1);
+    }
+    33% {
+      transform: scaleY(0.6);
+    }
+    50% {
+      transform: scaleY(0.2);
+    }
+    66% {
+      transform: scaleY(0.06);
+    }
+    83% {
+      transform: scaleY(0.06);
+    }
+    100% {
+      transform: scaleY(0.06);
+    }
+  }
+  @keyframes spike2 {
+    0% {
+      transform: scaleY(0.06);
+    }
+    16% {
+      transform: scaleY(0.06);
+    }
+    33% {
+      transform: scaleY(1);
+    }
+    50% {
+      transform: scaleY(0.6);
+    }
+    66% {
+      transform: scaleY(0.2);
+    }
+    83% {
+      transform: scaleY(0.06);
+    }
+    100% {
+      transform: scaleY(0.06);
+    }
+  }
+  @keyframes spike3 {
+    0% {
+      transform: scaleY(0.06);
+    }
+    16% {
+      transform: scaleY(0.06);
+    }
+    33% {
+      transform: scaleY(0.06);
+    }
+    50% {
+      transform: scaleY(1);
+    }
+    66% {
+      transform: scaleY(0.6);
+    }
+    83% {
+      transform: scaleY(0.2);
+    }
+    100% {
+      transform: scaleY(0.06);
+    }
+  }
+  @keyframes spike4 {
+    0% {
+      transform: scaleY(0.06);
+    }
+    16% {
+      transform: scaleY(0.06);
+    }
+    33% {
+      transform: scaleY(0.06);
+    }
+    50% {
+      transform: scaleY(0.06);
+    }
+    66% {
+      transform: scaleY(1);
+    }
+    83% {
+      transform: scaleY(0.6);
+    }
+    100% {
+      transform: scaleY(0.2);
+    }
+  }
+  @keyframes spike5 {
+    0% {
+      transform: scaleY(0.06);
+    }
+    16% {
+      transform: scaleY(0.06);
+    }
+    33% {
+      transform: scaleY(0.06);
+    }
+    50% {
+      transform: scaleY(0.06);
+    }
+    66% {
+      transform: scaleY(0.06);
+    }
+    83% {
+      transform: scaleY(1);
+    }
+    100% {
+      transform: scaleY(0.6);
+    }
+  }
+  @keyframes spike6 {
+    0% {
+      transform: scaleY(0.06);
+    }
+    16% {
+      transform: scaleY(0.06);
+    }
+    33% {
+      transform: scaleY(0.06);
+    }
+    50% {
+      transform: scaleY(0.06);
+    }
+    66% {
+      transform: scaleY(0.06);
+    }
+    83% {
+      transform: scaleY(0.6);
+    }
+    100% {
+      transform: scaleY(1);
+    }
+  }
+
+  @keyframes colorShift1 {
+    0% {
+      background-color: #91d1f1;
+    }
+    33.33% {
+      background-color: #f0a72f;
+    }
+    66.66% {
+      background-color: #f5f5f7;
+    }
+    100% {
+      background-color: #91d1f1;
+    }
+  }
+  @keyframes colorShift2 {
+    0% {
+      background-color: #91d1f1;
+    }
+    33.33% {
+      background-color: #f0a72f;
+    }
+    66.66% {
+      background-color: #f5f5f7;
+    }
+    100% {
+      background-color: #91d1f1;
+    }
+  }
+  @keyframes colorShift3 {
+    0% {
+      background-color: #25a5e6;
+    }
+    33.33% {
+      background-color: #ec6c33;
+    }
+    66.66% {
+      background-color: #f5f5f7;
+    }
+    100% {
+      background-color: #25a5e6;
+    }
+  }
+  @keyframes colorShift4 {
+    0% {
+      background-color: #25a5e6;
+    }
+    33.33% {
+      background-color: #e34534;
+    }
+    66.66% {
+      background-color: #5f5f63;
+    }
+    100% {
+      background-color: #25a5e6;
+    }
+  }
+  @keyframes colorShift5 {
+    0% {
+      background-color: #252fff;
+    }
+    33.33% {
+      background-color: #e34534;
+    }
+    66.66% {
+      background-color: #5f5f63;
+    }
+    100% {
+      background-color: #252fff;
+    }
+  }
+  @keyframes colorShift6 {
+    0% {
+      background-color: #252fff;
+    }
+    33.33% {
+      background-color: #cd18c2;
+    }
+    66.66% {
+      background-color: #5f5f63;
+    }
+    100% {
+      background-color: #252fff;
+    }
+  }
+</style>
--- components/Loader.vue
+++ components/Loader.vue
@@ -8,7 +8,7 @@
     >
       <Subtitle />
       <div class="h-[72px] md:h-[112px]">
-        <div class="loader-animation"></div>
+        <Animation />
       </div>
       <HeroTitle class="opacity-0" />
     </section>
@@ -16,173 +16,44 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from 'vue';
-import { useAppStore } from '~/stores/appStore';
+  import { ref, watch, onMounted } from 'vue';
+  import { useAppStore } from '~/stores/appStore';
 
-const appStore = useAppStore();
-const loader = ref(null);
-const load_Complete = ref(false);
-const emit = defineEmits(['loaded']);
+  const appStore = useAppStore();
+  const loader = ref(null);
+  const load_Complete = ref(false);
+  const emit = defineEmits(['loaded']);
 
-const nuxtApp = useNuxtApp();
+  const nuxtApp = useNuxtApp();
 
-nuxtApp.hook('app:suspense:resolve', () => {
-  load_Complete.value = true;
-});
+  nuxtApp.hook('app:suspense:resolve', () => {
+    load_Complete.value = true;
+  });
 
-const startAnimations = () => {
-  if (load_Complete.value) {
-    useGSAP().to(loader.value, {
-      delay: 1.5,
-      duration: 1,
-      opacity: 0,
-      ease: 'power4.inOut',
-      onComplete: () => {
-        appStore.pageLoaded = true;
-        emit('loaded');
-        document.body.style.overflow = ''; // re-enable scroll
-      },
-    });
-  }
-};
+  const startAnimations = () => {
+    if (load_Complete.value) {
+      useGSAP().to(loader.value, {
+        delay: 3,
+        duration: 1,
+        opacity: 0,
+        ease: 'power4.inOut',
+        onComplete: () => {
+          appStore.pageLoaded = true;
+          emit('loaded');
+          document.body.style.overflow = ''; // re-enable scroll
+        },
+      });
+    }
+  };
 
-onMounted(() => {
-  document.body.style.overflow = 'hidden'; // disable scroll while loading
-  startAnimations();
-});
-
-watch(load_Complete, (val) => {
-  if (val) {
+  onMounted(() => {
+    document.body.style.overflow = 'hidden'; // disable scroll while loading
     startAnimations();
-  }
-});
+  });
+
+  watch(load_Complete, (val) => {
+    if (val) {
+      startAnimations();
+    }
+  });
 </script>
-
-<style scoped>
-  .loader-animation {
-    position: relative;
-    animation:
-      rikSpikeRoll 0.65s linear infinite alternate,
-      colorChange 4s linear infinite;
-    background-image:
-      linear-gradient(#91d1f1 50px, transparent 0),
-      linear-gradient(#91d1f1 50px, transparent 0),
-      linear-gradient(#25a5e6 50px, transparent 0),
-      linear-gradient(#25a5e6 50px, transparent 0),
-      linear-gradient(#252fff 50px, transparent 0),
-      linear-gradient(#252fff 50px, transparent 0);
-    background-position:
-      0px center,
-      15px center,
-      30px center,
-      45px center,
-      60px center,
-      75px center,
-      90px center;
-    background-repeat: no-repeat;
-    width: 85px;
-    height: 50px;
-  }
-
-  @keyframes rikSpikeRoll {
-    0% {
-      background-size: 10px 3px;
-    }
-    16% {
-      background-size:
-        10px 50px,
-        10px 3px,
-        10px 3px,
-        10px 3px,
-        10px 3px,
-        10px 3px;
-    }
-    33% {
-      background-size:
-        10px 30px,
-        10px 50px,
-        10px 3px,
-        10px 3px,
-        10px 3px,
-        10px 3px;
-    }
-    50% {
-      background-size:
-        10px 10px,
-        10px 30px,
-        10px 50px,
-        10px 3px,
-        10px 3px,
-        10px 3px;
-    }
-    66% {
-      background-size:
-        10px 3px,
-        10px 10px,
-        10px 30px,
-        10px 50px,
-        10px 3px,
-        10px 3px;
-    }
-    83% {
-      background-size:
-        10px 3px,
-        10px 3px,
-        10px 10px,
-        10px 30px,
-        10px 50px,
-        10px 3px;
-    }
-    100% {
-      background-size:
-        10px 3px,
-        10px 3px,
-        10px 3px,
-        10px 10px,
-        10px 30px,
-        10px 50px;
-    }
-  }
-
-  /* Color change animation */
-  @keyframes colorChange {
-    0% {
-      background-image:
-        linear-gradient(#91d1f1 50px, transparent 0),
-        linear-gradient(#91d1f1 50px, transparent 0),
-        linear-gradient(#25a5e6 50px, transparent 0),
-        linear-gradient(#25a5e6 50px, transparent 0),
-        linear-gradient(#252fff 50px, transparent 0),
-        linear-gradient(#252fff 50px, transparent 0);
-    }
-
-    33.33% {
-      background-image:
-        linear-gradient(#f0a72f 50px, transparent 0),
-        linear-gradient(#f0a72f 50px, transparent 0),
-        linear-gradient(#ec6c33 50px, transparent 0),
-        linear-gradient(#e34534 50px, transparent 0),
-        linear-gradient(#e34534 50px, transparent 0),
-        linear-gradient(#cd18c2 50px, transparent 0);
-    }
-
-    66.66% {
-      background-image:
-        linear-gradient(#f5f5f7 50px, transparent 0),
-        linear-gradient(#f5f5f7 50px, transparent 0),
-        linear-gradient(#f5f5f7 50px, transparent 0),
-        linear-gradient(#5f5f63 50px, transparent 0),
-        linear-gradient(#5f5f63 50px, transparent 0),
-        linear-gradient(#5f5f63 50px, transparent 0);
-    }
-    100% {
-      background-image:
-        linear-gradient(#91d1f1 50px, transparent 0),
-        linear-gradient(#91d1f1 50px, transparent 0),
-        linear-gradient(#25a5e6 50px, transparent 0),
-        linear-gradient(#25a5e6 50px, transparent 0),
-        linear-gradient(#252fff 50px, transparent 0),
-        linear-gradient(#252fff 50px, transparent 0);
-    }
-  }
-</style>
